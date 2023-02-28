d23b2865e41e8d0c36c1bf3b59c150d870789e8e
r1hardz <120601380+r1hardz@users.noreply.github.com>
2023-02-28 04:25:18 -0800

    Create pw2_2.py

---
+++ Assignment_2/pw2_2.py
@@ -0,0 +1,21 @@
+def compute_salary(hours,rate):
+    if (hours > 40):
+        extraHours = hours - 40
+        increasedRate = rate * 1.75
+        basePayment = 40 * rate
+        extraPayment = extraHours * increasedRate
+        total = basePayment + extraPayment
+    else:
+        total = hours * rate
+    print("Pay: " + str(total))
+
+if __name__ == "__main__":
+    while True:
+        try:
+            hours = float(input("Enter hours: "))
+            rate = float(input("Enter rate: "))
+            compute_salary(hours,rate)
+            break
+        except ValueError:
+            print("Error, please enter numeric input")
+            continue
